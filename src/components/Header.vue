6bfc134714d4e816e5388a6c75312ed60d4736c3
barclayiversen <barclayiversen@gmail.com>
2019-07-30 15:37:04 -0700

    add gitignore

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link to="/portfolio" activeClass="active" tag="li">
+            <a>Portfolio</a>
+          </router-link>
+          <router-link to="/stocks" activeClass="active" tag="li">
+            <a>Stocks</a>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">End Day</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
